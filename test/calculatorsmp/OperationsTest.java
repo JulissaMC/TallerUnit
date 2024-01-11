6c9254baaf3839d428ccfaa20d5ef4058ea4b01b
AlexitoSnow <ganieves@espol.edu.ec>
2024-01-10 23:41:31 -0500

    Pruebas unitarias para los casos faltantes

--- test/calculatorsmp/OperationsTest.java
+++ test/calculatorsmp/OperationsTest.java
@@ -1,93 +1,114 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
- */
 package calculatorsmp;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-/**
- *
- * @author Olivier MZ
- */
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class OperationsTest {
-    
-    private Operations calculadora;
+
     private String formula;
+
     public OperationsTest() {
     }
-    
-    @BeforeAll
-    public static void setUpClass() {
-    }
-    
-    @AfterAll
-    public static void tearDownClass() {
-    }
-    
+
+    /**
+     * Genera una fórmula aleatoria antes de cada test
+     */
     @BeforeEach
+    @DisplayName("Generar fórmula")
     public void setUp() {
-        calculadora = new Operations();
-    }
-    
-    @AfterEach
-    public void tearDown() {
+        formula = Operations.MakeFormula();
+        System.out.println("Formula generada: " + formula);
+        notNull();
     }
 
-    /**
-     * Test of MakeFormula method, of class Operations.
-     */
+    // Test del método MakeFormula
+
     @Test
-    public void testMakeFormula() {
-        System.out.println("MakeFormula");
-        String expResult = "";
-        String result = Operations.MakeFormula();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    @DisplayName("Estructura correcta de la fórmula")
+    public void testEstructuraCorrecta() {
+        assertTrue(formula.matches("(\\d+[-+*/])+\\d+"));
+        System.out.println("Formato correcto: " + formula);
     }
 
-    /**
-     * Test of Solve method, of class Operations.
-     */
     @Test
-    public void testSolve() {
-        System.out.println("Solve");
-        String formula = "";
-        String expResult = "";
-        String result = Operations.Solve(formula);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    @DisplayName("No empieza con operador")
+    public void testNoEmpiezaConOperador() {
+        assertTrue(formula.matches("^[^*+/\\\\-].*"));
     }
-    
+
     @Test
-    public void notNull(){
+    @DisplayName("Devuelve al menos dos números para las operaciones")
+    public void testAlMenosDosNumerosParaOperar() {
+        assertTrue(formula.matches(".*\\d+.*\\d+.*"));
+    }
+
+    @Test
+    public void notNull() {
         assertNotNull(formula);
     }
-    
+
     @Test
-    public void notEmpty(){
+    public void notEmpty() {
         assertFalse(formula.isEmpty());
     }
-    
-    @Test
-    public void EstructuraCorrecta(){
-        assertTrue(formula.matches("\\d+[+\\-\\*]\\d+"));
+
+    /**
+     * Pone en null a la fórmula después de cada test
+     */
+    @AfterEach
+    @DisplayName("Pone en null a la formula")
+    public void tearDown() {
+        formula = null;
     }
-    
+
+    // Test del método Solve
+
     @Test
-    public void noGenerarNumerosNegativos(){
-        assertFalse(formula.matches(".*\\-\\d+.*"));
+    @DisplayName("Valor esperado")
+    public void testValorEsperado() {
+        String formula = "42+54*12/3";
+        String expResult = formula + "=258";
+        String result = Operations.Solve(formula);
+        assertEquals(expResult, result);
+        System.out.println("Valor esperado: " + result);
+        System.out.println("Valor obtenido: " + expResult);
     }
-    
+
     @Test
-    public void alMenosDosNumerosParaOperar(){
-        assertTrue(formula.matches(".*\\d+.*\\d+.*"));
+    @DisplayName("Arroja excepción cuando la fórmula está vacía")
+    public void testFormulaVacia() {
+        String formula = "";
+        assertThrows(java.util.EmptyStackException.class, () -> Operations.Solve(formula));
+    }
+
+    @Test
+    @DisplayName("Arroja excepción en una división para cero")
+    public void testDivisionParaCero() {
+        String formula = "10/0";
+        assertThrows(ArithmeticException.class, () -> Operations.Solve(formula));
+    }
+
+    @Test
+    @DisplayName("Resuelve con números negativos")
+    public void testResuelveConValoresNegativos() {
+        String formula = "-2+2";
+        String expected = formula + "=0";
+        String result = Operations.Solve(formula);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    @DisplayName("Arroja null pointer exception cuando la fórmula es null")
+    public void testFormulaNula() {
+        String formula = null;
+        assertThrows(NullPointerException.class, () -> Operations.Solve(formula));
     }
 }
